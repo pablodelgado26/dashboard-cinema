63bd18643239eb05981f9ac9f5f0a115530d0050
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-03-11 09:35:01 -0300

    feat: criação de componente Sidebar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
 import Header from "@/components/header";
 import styles from "./page.module.css";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <main className={styles.dashboardContainer}>
+      <Sidebar />
+
+      </main>
     </div>
   );
 }
